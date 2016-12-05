75781646252127f8ad308aa01d8f2a32843ea01e
Unkyu Lee <unkyulee@gmail.com>
2016-12-05 15:05:16 +0100

    delete site was not working

--- src/web/modules/admin/controllers/site/delete.py
+++ src/web/modules/admin/controllers/site/delete.py
@@ -3,11 +3,18 @@ from lib import es
 import web.util.tools as tools
 
 def get(p):
-    # root site can't be edited
-    if request.form["id"] == "0":
+    site_id = p['nav'][-1]
+
+    # root site can't be deleted
+    if site_id == "0":
         return tools.alert("Root site can't be deleted")
-    else:
-        es.delete(p['host'], 'core_nav', 'site', request.form['id'])
-        es.flush(p['host'], 'core_nav')
+
+    # check if site id is valid
+    site = es.get(p['host'], 'core_nav', 'site', site_id)
+    if not site:
+        return tools.alert("not valid site id: {}".format(site_id))
+
+    es.delete(p['host'], 'core_nav', 'site', site_id)
+    es.flush(p['host'], 'core_nav')
 
     return tools.redirect(request.referrer)
